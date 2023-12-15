ced6bc6ee545845e230352f050051790d9a56809
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2023-12-15 14:41:38 +0100

    creazione classi

--- src/main/java/laura/gallace/entities/Libro.java
+++ src/main/java/laura/gallace/entities/Libro.java
@@ -1,4 +1,47 @@
 package laura.gallace.entities;
 
-public class Libro {
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="libro")
+@DiscriminatorValue("libro")
+public class Libro extends ElementoBibliotecario {
+    private String autore;
+    private String genere;
+
+
+    public void libro(){
+
+    }
+    public Libro(String titolo,int annoPubblicazione, int numeroPagine, String autore, String genere ) {
+        super(titolo, annoPubblicazione, numeroPagine);
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "autore='" + autore + '\'' +
+                ", genere='" + genere + '\'' +
+                '}';
+    }
 }
--- src/main/java/laura/gallace/entities/Rivista.java
+++ src/main/java/laura/gallace/entities/Rivista.java
@@ -1,4 +1,36 @@
 package laura.gallace.entities;
 
-public class Rivista {
+import laura.gallace.entities.enums.Periodicità;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="rivista")
+@DiscriminatorValue("rivista")
+public class Rivista extends ElementoBibliotecario {
+    private Periodicità periodicità;
+    public Rivista(){
+}
+
+    public Rivista(String titolo, int annoPubblicazione, int numeroPagine, Periodicità periodicità) {
+        super(titolo, annoPubblicazione, numeroPagine);
+        this.periodicità = periodicità;
+    }
+
+    public Periodicità getPeriodicità() {
+        return periodicità;
+    }
+
+    public void setPeriodicità(Periodicità periodicità) {
+        this.periodicità = periodicità;
+    }
+
+    @Override
+    public String toString() {
+        return "Rivista{" +
+                "periodicità=" + periodicità +
+                '}';
+    }
 }
